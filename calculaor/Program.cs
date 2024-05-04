87c2cc16257c0499b8a76cac826719b7663256af
Nutan-Gyawali <nutangyawali01@gmail.com>
2024-05-04 14:02:05 +0545

    Add project files.

---
+++ calculaor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.Design;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("opeator");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.ReadLine();
+
+            if (op == "+") 
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            
+            else if(op == "-") {
+                Console.WriteLine(num1-num2);
+
+
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 *num2);
+
+
+            }
+            else
+            {
+                Console.WriteLine(num2 / num1);
+            }
+        }
+    }
+}
